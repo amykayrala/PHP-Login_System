68745a0d36b5eedfe25da1abafe81e5c00baa52b
Amy <102492396+amykayrala@users.noreply.github.com>
2022-03-27 22:52:46 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+
+<script src="https://cdnjs.cloudfare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.13.4/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.13.4/dist/js/uikit-icons.min.js"></script>
