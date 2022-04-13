530b090672e2ac410621ad8e0a98de7144e249fe
Amy <102492396+amykayrala@users.noreply.github.com>
2022-04-13 10:43:09 -0400

    Delete footer.php

--- inc/footer.php
+++
@@ -1,6 +0,0 @@
-
-<script src="https://cdnjs.cloudfare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-
-<script src="https://cdn.jsdelivr.net/npm/uikit@3.13.4/dist/js/uikit.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/uikit@3.13.4/dist/js/uikit-icons.min.js"></script>
